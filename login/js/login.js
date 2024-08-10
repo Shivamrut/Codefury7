dbed96077e29d6b240070b697650d4fdf6b4f551
maxus_red <incineroaraditya@gmail.com>
2024-08-10 23:35:54 +0530

    login added nav foot

--- login/js/login.js
+++ login/js/login.js
@@ -1,7 +1,6 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
@@ -57,7 +56,7 @@ submitLogIn.addEventListener("click", function (event) {
       const user = userCredential.user;
       alert("Logging In...")
       alert("Login  successful")
-      window.location.href = "index.html"
+      window.location.href = "../../index.html"
       // ...
     })
     .catch((error) => {
